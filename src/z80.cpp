6ca556b09ff9131b45e07c7653eaf26fdb5a8db4
Alec Chan <me@alecchan.io>
2018-05-18 16:08:24 -0700

    implemented cmpr_b and NOP instructions

--- src/z80.cpp
+++ src/z80.cpp
@@ -19,5 +19,20 @@ void z80::addr_e() {
  * Compare register B to A, and set flag
  **/
 void z80::cmpr_b() {
-    
+    unsigned char temp_a = this->_r.a;
+    temp_a -= this->_r.b;
+    this->_r.f |= 0x40; // set the subtraction flag
+    if(!(temp_a & 255)) this->_r.f |= 0x80;
+    if(temp_a < 0) this->_r.f |= 0x10;
+    this->_r.m = 1;
+    this->_r.t = 4;
+}
+
+/**
+ * Implement the NOP no-operation instruction
+ * Simply passes time
+ **/
+void z80::NOP() {
+    this->_r.m = 1;
+    this->_r.t = 4;
 }
