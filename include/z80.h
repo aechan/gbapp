b7ec870fa68dbbaf0f128ad24a8ece401c907c30
Alec Chan <me@alecchan.io>
2018-05-19 16:40:08 -0700

    added function declarations for more z80 instruction sets, specifically data/loading ones.

--- include/z80.h
+++ include/z80.h
@@ -42,8 +42,95 @@ class z80 {
 public:
     zclock _c;
     registers _r;
-    void addr_e();  // add registr E to A leave result in A (ADD A, E)
-    void cmpr_b();  // compare B to A, set flags (CP A, B)
+    
+    /**
+     * Misc instructions
+     **/ 
     void NOP();     // NOP - no operation
     void reset();   // resets cpu for startup
+
+    /**
+     * Data load/store
+     **/ 
+    // Register A
+    void ldr_aa();
+    void ldr_ab();
+    void ldr_ac();
+    void ldr_ad();
+    void ldr_ae();
+    void ldr_ah();
+    void ldr_al();
+
+    // Register B
+    void ldr_ba();
+    void ldr_bb();
+    void ldr_bc();
+    void ldr_bd();
+    void ldr_be();
+    void ldr_bh();
+    void ldr_bl();
+
+    // Register C
+    void ldr_ca();
+    void ldr_cb();
+    void ldr_cc();
+    void ldr_cd();
+    void ldr_ce();
+    void ldr_ch();
+    void ldr_cl();
+
+    // Register D
+    void ldr_da();
+    void ldr_db();
+    void ldr_dc();
+    void ldr_dd();
+    void ldr_de();
+    void ldr_dh();
+    void ldr_dl();
+
+    // Register E
+    void ldr_ea();
+    void ldr_eb();
+    void ldr_ec();
+    void ldr_ed();
+    void ldr_ee();
+    void ldr_eh();
+    void ldr_el();
+
+    // Register H
+    void ldr_ha();
+    void ldr_hb();
+    void ldr_hc();
+    void ldr_hd();
+    void ldr_he();
+    void ldr_hh();
+    void ldr_hl();
+
+    // Register L
+    void ldr_la();
+    void ldr_lb();
+    void ldr_lc();
+    void ldr_ld();
+    void ldr_le();
+    void ldr_lh();
+    void ldr_ll();
+
+    /**
+     * Pointer loaders
+     **/ 
+    void ldr_HLa();
+    void ldr_HLb();
+    void ldr_HLc();
+    void ldr_HLd();
+    void ldr_HLe();
+    void ldr_HLh();
+    void ldr_HLl();
+
+
+    /**
+     * Data processors
+     **/ 
+    void cmpr_b();  // compare B to A, set flags (CP A, B)
+    void addr_e();  // add registr E to A leave result in A (ADD A, E)
+
 };
